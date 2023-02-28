959bbaa407a824a73f931f9aef63794815f3e118
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-02-28 11:48:22 +0200

    homepage WIP

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,13 +1,53 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { Image, SafeAreaView, Text, TextInput, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      // headerTitle: "Deliveroo",
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-green-500">HOME</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-white pt-14">
+      {/* by default View is a flex column container */}
+      <View className="flex flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="w-7 h-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00ccbb" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00ccbb" />
+      </View>
+
+      <View className="flex flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex flex-row space-x-2 flex-1 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" />
+          <TextInput
+            placeholder="Restaurants and cousines"
+            keyboard="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00ccbb" />
+      </View>
+    </SafeAreaView>
   );
 };
 
